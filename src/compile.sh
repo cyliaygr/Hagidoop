0e4d91b16391b126aafdab01ce64d0e3054ce7c3
Daamms <damien.brop@gmail.com>
2024-01-12 09:14:58 +0100

    Create compile.sh

---
+++ src/compile.sh
@@ -0,0 +1,13 @@
+echo "Debut de compilation "
+javac application/*
+echo " - application ok "
+javac config/*
+echo " - configok "
+javac daemon/*
+echo " - daemon ok "
+javac hdfs/*
+echo " - hdfs ok "
+javac interfaces/*
+echo " - interface ok "
+echo "Compilation fini "
+
